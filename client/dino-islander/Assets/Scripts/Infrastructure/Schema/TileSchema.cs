1f9b51b4bf8819aed57593b9a089099539bf4e5e
Wenish <Wenish@users.noreply.github.com>
2026-02-15 22:29:17 +0100

    Add generated C# schemas and update server
    Add Colyseus-generated C# schema classes (GameRoomState.cs, TileSchema.cs) with Unity .meta files and folder meta under client/.../Infrastructure/Schema. Add an npm script "generate-schema-unity" to server/package.json to automate schema codegen. Remove the unused hiddenCards array from server/src/schema/GameRoomState.ts. Also update ProjectSettings/GraphicsSettings.asset URP pipeline GUID. These changes sync client schema types with the server and enable easy regeneration of the C# schemas.

---
+++ client/dino-islander/Assets/Scripts/Infrastructure/Schema/TileSchema.cs
@@ -0,0 +1,28 @@
+// 
+// THIS FILE HAS BEEN GENERATED AUTOMATICALLY
+// DO NOT CHANGE IT MANUALLY UNLESS YOU KNOW WHAT YOU'RE DOING
+// 
+// GENERATED USING @colyseus/schema 4.0.12
+// 
+
+using Colyseus.Schema;
+#if UNITY_5_3_OR_NEWER
+using UnityEngine.Scripting;
+#endif
+
+namespace DinoIslander.Infrastructure {
+	public partial class TileSchema : Schema {
+#if UNITY_5_3_OR_NEWER
+[Preserve]
+#endif
+public TileSchema() { }
+		[Type(0, "uint16")]
+		public ushort x = default(ushort);
+
+		[Type(1, "uint16")]
+		public ushort y = default(ushort);
+
+		[Type(2, "string")]
+		public string type = default(string);
+	}
+}
--- server/src/schema/GameRoomState.ts
+++ server/src/schema/GameRoomState.ts
@@ -25,7 +25,4 @@ export class GameRoomState extends Schema {
 
   @type([TileSchema])
   tiles = new ArraySchema<TileSchema>();
-
-
-  hiddenCards = new ArraySchema<TileSchema>();
 }
